3cd99895a3cb327027351b00c67ce37aa22542e4
Jeremy Hull <jchull@gmail.com>
2020-04-04 09:40:04 -0600

    update packages, fix webpack config for local serve

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-var webpack = require('webpack'),
-  path = require('path');
+const ROOT = path.resolve(__dirname);
 
-var ROOT = path.resolve(__dirname);
+const APP = __dirname + '/app';
 
-var APP = __dirname + '/app';
-
-var config = {
+const config = {
   context: APP,
   entry: {
     app: './index.js'
@@ -17,107 +16,64 @@ var config = {
     publicPath: '/',
     filename: 'presentation.bundle.js'
   },
-
-  resolve: {
-    // the fewer directories, the faster module resolution is
-    modules: ['./node_modules'],
-    alias: {
-    }
-  },
-
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [{
-          loader: MiniCssExtractPlugin.loader
-        },
-          'css-loader'
-        ]
+        test: require.resolve('reveal.js'),
+        use: {
+          loader: 'expose-loader',
+          options: 'Reveal'
+        }
       },
       {
         test: /\.eot(\?\S*)?$/,
-        use: [{
-          loader: 'url-loader?limit=100000&mimetype=application/vnd.ms-fontobject'
-        }
-        ]
+        use: 'url-loader?limit=100000&mimetype=application/vnd.ms-fontobject'
       },
       {
         test: /\.woff2(\?\S*)?$/,
-        use: [{
-          loader: 'url-loader?limit=100000&mimetype=application/font-woff2'
-        }
-        ]
+        use: 'url-loader?limit=100000&mimetype=application/font-woff2'
       },
       {
         test: /\.woff(\?\S*)?$/,
-        use: [{
-          loader: 'url-loader?limit=100000&mimetype=application/font-woff'
-        }
-        ]
+        use: 'url-loader?limit=100000&mimetype=application/font-woff'
       },
       {
         test: /\.ttf(\?\S*)?$/,
-        use: [{
-          loader: 'url-loader?limit=100000&mimetype=application/font-ttf'
-        }
-        ]
+        use: 'url-loader?limit=100000&mimetype=application/font-ttf'
       },
       {
         test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader'
-        }
-        ]
+        use: 'file-loader'
       },
       {
         test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
-        }
-        ]
+        use: 'svg-url-loader?limit=10000&mimetype=image/svg+xml'
       },
       {
         test: /\.html$/,
-        use: [{
-          loader: 'html-loader'
-        }
-        ]
+        use: 'html-loader'
       },
       {
         test: /\.jpg$/,
-        use: [{
-          loader: 'file-loader'
-        }
-        ]
+        use: 'file-loader'
       },
       {
         test: /\.png$/,
-        use: [{
-          loader: 'url-loader?mimetype=image/png'
-        }
-        ]
+        use: 'url-loader?mimetype=image/png'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /index\.html/,
-        use: [{
-          loader: 'file-loader?name=[name].[ext]'
-        }
-        ]
+        use: 'file-loader?name=[name].[ext]'
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      // filename: "[name].css",
-      // chunkFilename: "[id].css"
-    }),
-    new webpack.ProvidePlugin({
-      'Reveal': 'reveal.js'
-    }),
-
+    new CleanWebpackPlugin({}),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     // use for development time hot-swap of only modified modules that the webpack client will load up
     new webpack.HotModuleReplacementPlugin()
   ],
